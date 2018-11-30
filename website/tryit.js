66a845b9b43cd24d15fd1b14448c162d20404b4a
sfplayer247 <pauloneill247@gmail.com>
2018-11-30 16:20:25 -0500

    Added execution and parse time measurements to the interpreter.

--- website/tryit.js
+++ website/tryit.js
@@ -2,12 +2,27 @@ import Enviroment from "../src/env.js";
 
 function runcode() {
   var code = document.getElementById("input").value;
-  if (document.getElementById('parseCheckbox').checked) {
-    console.dir(new Enviroment().parse(code))
+
+  var env = new Enviroment();
+
+  var start = window.performance.now();
+  var ast = env.parse(code);
+  var end = window.performance.now();
+
+  var parseTime = end - start;
+  if (document.getElementById("parseCheckbox").checked) {
+    console.dir(ast);
   }
-  document.getElementById("output").innerText = new Enviroment().run(
-    code
-  ).output;
+
+  start = window.performance.now();
+  env.execute(ast, env);
+  end = window.performance.now();
+
+  document.getElementById("output").innerText = env.output;
+  document.getElementById("time").innerText = `
+  Time to parse: ${parseTime}ms
+  Time to execute: ${end - start}ms
+  Time to parse & execute: ${parseTime + end - start}ms`;
 }
 
 // Load any example code
