43fb1637c1987a798fb1aee41c5f9d4315a87ab8
sfplayer247 <pauloneill247@gmail.com>
2018-11-30 11:25:00 -0500

    Added Option To Output the AST on the interpreter.

--- website/tryit.js
+++ website/tryit.js
@@ -2,10 +2,14 @@ import Enviroment from "../src/env.js";
 
 function runcode() {
   var code = document.getElementById("input").value;
-  console.log(new Enviroment().parse(code));
+  if (document.getElementById('parseCheckbox').checked) {
+    console.dir(new Enviroment().parse(code))
+  }
   document.getElementById("output").innerText = new Enviroment().run(
     code
   ).output;
 }
+
+// Load any example code
 document.getElementById("input").value = localStorage.getItem("tryitView");
 document.getElementById("runcode").addEventListener("click", runcode);
